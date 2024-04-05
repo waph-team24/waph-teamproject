8edd5d0bb04ee309d87288a7c21571d1cf612701
sanipidr <sanipidr@mail.uc.edu>
2024-04-05 01:30:31 -0400

    waph_team

---
+++ database/database-account.sql
@@ -0,0 +1,3 @@
+create database waph_team;
+CREATE USER 'sanipidr'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
+GRANT ALL ON waph.*TO'sanipidr'@'localhost';
---
+++ database/database-data.sql
@@ -0,0 +1,3 @@
+create database waph;
+CREATE USER 'sanipidr'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
+GRANT ALL ON waph.*TO'sanipidr'@'localhost';
