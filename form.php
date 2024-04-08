769f6ca0ff638d6b57e9c0b3e7f5232bb62b2c56
gaddesr <gaddesr@mail.uc.edu>
2024-04-08 01:59:44 -0400

    form&index

---
+++ form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>WAPH Team24-Login page</title>
+  <script type="text/javascript">
+      function displayTime() {
+        document.getElementById('digit-clock').innerHTML = "Current time:" + new Date();
+      }
+      setInterval(displayTime,500);
+  </script>
+</head>
+<body>
+  <h1>Mini Facebook Login Form</h1>
+  <h2>Student Name</h2>
+  <div id="digit-clock"></div>  
+<?php
+  //some code here
+  echo "Visited time: " . date("Y-m-d h:i:sa")
+?>
+  <form action="index.php" method="POST" class="form login">
+    Username:<input type="text" class="text_field" name="username" /> <br>
+    Password: <input type="password" class="text_field" name="password" /> <br>
+    <button class="button" type="submit">Login</button>
+  </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+	session_start();    
+	if (checklogin_mysql($_POST["username"],$_POST["password"])) {
+?>
+	<h2> Welcome <?php echo $_POST['username']; ?> !</h2>
+<?php		
+	}else{
+		echo "<script>alert('Invalid username/password');window.location='form.php';</script>";
+		die();
+	}
+	function checklogin($username, $password) {
+		$account = array("admin","1234");
+		if (($username== $account[0]) and ($password == $account[1])) 
+		  return TRUE;
+		else 
+		  return FALSE;
+  	}
+  	
